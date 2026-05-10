49c28933e4f93d789a205b22eb15e0400dd38f27
Suraz Agrahari <surazagrahari1980@gmail.com>
2026-05-10 23:30:25 +0545

    feat: update setNewName to include a greeting after name change

--- Learn/UpdateUser.jsx
+++ Learn/UpdateUser.jsx
@@ -8,7 +8,7 @@ const UpdateUser = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     updateName({ name: newName });
-    setNewName("");
+    setNewName(" hello");
   };
   return (
     <div>
